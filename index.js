2ee00303177987c9ee658c5e0e9d14b8e429a1a4
ShadowOfDeath25 <abdo.251152@gmail.com>
2026-08-25 17:28:43 +0300

    updated structure

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-import Fastify from 'fastify'
-
-const fastify = Fastify({
-    logger: true
-})
-
-
-fastify.get('/', function (request, reply) {
-    reply.send({ hello: 'world' })
-})
-
-fastify.listen({ port: 3000 }, function (err, address) {
-    if (err) {
-        fastify.log.error(err)
-        process.exit(1)
-    }
-
-})
+import "./src/server.js"
