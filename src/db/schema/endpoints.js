c51937c3f59d575144db7c0f1b53f662d404f5da
ShadowOfDeath25 <abdo.251152@gmail.com>
2026-08-31 12:43:12 +0300

    feat(endpoints): Add URL format validation check (db/schema)
    Introduces a new check constraint to the `endpoints` table to enforce
    a valid URL format for the `url` column.
    
    - Defines a URL_REGEX constant for comprehensive URL validation.
    - Applies this regex as a 'endpoints_url_format_check' constraint.
    ```

--- src/db/schema/endpoints.js
+++ src/db/schema/endpoints.js
@@ -1,8 +1,8 @@
-import {index,check, pgTable} from "drizzle-orm/pg-core";
+import {index, check, pgTable} from "drizzle-orm/pg-core";
 import {consumers} from './consumers.js';
 import {sql} from 'drizzle-orm'
 
-
+const URL_REGEX = /^https?:\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)$/g;
 
 export const endpoints = pgTable("endpoints", (t) => ({
     id: t.integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -15,6 +15,13 @@ export const endpoints = pgTable("endpoints", (t) => ({
     index("endpoints_consumer_id_fk_idx").on(table.consumerId),
     check(
         'endpoints_signing_key_format_check',
-        sql`${table.signingKey} ~* '^_hs_'`
+        sql`${table.signingKey}
+        ~* '^_hs_'`
+    ),
+    check(
+        'endpoints_url_format_check',
+        sql`${table.url}
+        ~
+        ${URL_REGEX.source}`
     )
 ])
