11b251f948ad00e0302cdfae0a46ce26ed3a5686
MaroB05 <s-marwan.hussien@zewailcity.edu.eg>
2026-09-02 03:30:53 +0300

    implemented data schemas

---
+++ src/events/events.model.js
@@ -0,0 +1,28 @@
+const eventPayloadSchema = {
+    type: 'object',
+    required: ['timestamp', 'type', 'data'],
+    properties: {
+        timestamp: { type: 'string', format: 'date-time' },
+        type: { type: 'string' },
+        data: { type: 'object' }
+    }
+};
+
+export const eventBodySchema = {
+    type: 'object',
+    required: ['consumerID', 'eventData'],
+    properties: {
+        consumerID: { type:'string' },
+        eventData: eventPayloadSchema
+    }
+};
+
+export const eventinQueueSchema = {
+    type: 'object',
+    required: ['event_id', 'payload', 'endpoint_id'],
+    properties: {
+        event_id: { type: 'string', required: true },
+        payload: eventPayloadSchema,
+        endpoint_id: { type: 'string', required: true }
+    }
+}
