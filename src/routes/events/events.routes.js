9d356e071ae06422d37ff902c7bacf59cb43a1da
MaroB05 <s-marwan.hussien@zewailcity.edu.eg>
2026-09-02 12:42:43 +0300

    renamed from events.route to events.routes and updated imports

--- src/routes/events/events.routes.js
+++ src/routes/events/events.routes.js
@@ -1,8 +1,8 @@
-import { createEvent } from './events.handlers.js';
+import { createEvent } from './events.services.js';
 import { eventBodySchema } from './events.schemas.js';
 
 export default async function eventRoutes(fastify) {
-    fastify.post('events/', {
+    fastify.post('/', {
         schema: {
             body: eventBodySchema
         }
