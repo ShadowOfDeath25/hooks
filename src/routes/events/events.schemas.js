3d2db49b12d316f99ef34195ad1da058c83776e5
MaroB05 <s-marwan.hussien@zewailcity.edu.eg>
2026-09-02 12:22:52 +0300

    updated id datat type to integer with minimum 1

--- src/routes/events/events.schemas.js
+++ src/routes/events/events.schemas.js
@@ -12,17 +12,17 @@ export const eventBodySchema = {
     type: 'object',
     required: ['consumerID', 'eventData'],
     properties: {
-        consumerID: { type:'number' },
+        consumerID: { type:'integer', minimum: 1 },
         eventData: eventPayloadSchema
     }
 };
 
-export const eventinQueueSchema = {
+export const enqueuedEventSchema = {
     type: 'object',
     required: ['event_id', 'payload', 'endpoint_id'],
     properties: {
-        event_id: { type: 'number', required: true },
+        event_id: { type: 'number', minimum: 1 },
         payload: eventPayloadSchema,
-        endpoint_id: { type: 'number', required: true }
+        endpoint_id: { type: 'number', minimum: 1 }
     }
 }
