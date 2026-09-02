ac2e1625ee21bf57b3782b4041cbeb2a11cbc779
MaroB05 <s-marwan.hussien@zewailcity.edu.eg>
2026-09-02 12:43:20 +0300

    moved createEvent to a service

--- src/routes/events/events.handlers.js
+++ src/routes/events/events.handlers.js
@@ -1,14 +1,10 @@
 import { eq } from 'drizzle-orm';
 import { db } from '../../db/index.js';
-import { dummyQueue } from '../../worker.js';
-import { events } from '../../db/schema/events.js';
 import { consumers } from '../../db/schema/consumers.js';
-import { endpoints } from '../../db/schema/endpoints.js';
-import { deliveries } from '../../db/schema/deliveries.js';
 
 
 // initial validation is done by fastify schema validation
-async function validatePayload(payload) {
+export async function validatePayload(payload) {
     const { consumerID, eventData } = payload;
     
     // Validate eventData size (max 1MB)
@@ -22,61 +18,5 @@ async function validatePayload(payload) {
 
     if (!consumer) {
         throw new Error(`Consumer with ID ${consumerID} does not exist`);
-    }
-}
-
-export async function createEvent(request, reply) {
-    const payload = request.body;
-    const { consumerID, eventData } = payload;
-
-    try {
-        await validatePayload(payload);
-    } catch (error) {
-        return reply.code(400).send({
-            success: false,
-            message: error.cause.message ?? error.message,
-            stack: error.stack
-        });
-    }
-
-    try {
-        // Insert event into database
-        const [createdEvent] = await db.insert(events).values({
-            type: eventData.type,
-            payload: eventData,
-            consumerId: consumerID
-        }).returning();
-        
-        const eventId = createdEvent.id;
-        const consumerEndpoints = await db.select().from(endpoints).where(eq(endpoints.consumerId, consumerID));
-        
-        // Create deliveries and enqueue jobs for each endpoint
-        for (const endpoint of consumerEndpoints) {
-
-            await db.insert(deliveries).values({
-                eventId: eventId,
-                endpointId: endpoint.id,
-                status: 'pending'
-            });
-
-            await dummyQueue.add('dummyQueue', {
-                event_id: eventId.toString(),
-                payload: eventData,
-                endpoint_id: endpoint.id.toString()
-            });
-        }
-
-        return reply.code(201).send({
-            success: true,
-            message: 'Event received',
-            event_id: eventId
-        });
-    } catch (error) {
-        return reply.code(500).send({
-            success: false,
-            message: 'Failed to process event',
-            error: error?.message,
-            stack: error.stack
-        });
     }
 }
---
+++ src/routes/events/events.services.js
@@ -0,0 +1,63 @@
+import { eq } from 'drizzle-orm';
+import { db } from '../../db/index.js';
+import { dummyQueue } from '../../worker.js';
+import { events } from '../../db/schema/events.js';
+import { endpoints } from '../../db/schema/endpoints.js';
+import { validatePayload } from './events.handlers.js';
+import { deliveries } from '../../db/schema/deliveries.js';
+
+export async function createEvent(request, reply) {
+    const payload = request.body;
+    const { consumerID, eventData } = payload;
+
+    try {
+        await validatePayload(payload);
+    } catch (error) {
+        return reply.code(400).send({
+            success: false,
+            message: error.cause.message ?? error.message,
+            stack: error.stack
+        });
+    }
+
+    try {
+        // Insert event into database
+        const [createdEvent] = await db.insert(events).values({
+            type: eventData.type,
+            payload: eventData,
+            consumerId: consumerID
+        }).returning();
+        
+        const eventId = createdEvent.id;
+        const consumerEndpoints = await db.select().from(endpoints).where(eq(endpoints.consumerId, consumerID));
+        
+        // Create deliveries and enqueue jobs for each endpoint
+        for (const endpoint of consumerEndpoints) {
+
+            await db.insert(deliveries).values({
+                eventId: eventId,
+                endpointId: endpoint.id,
+                status: 'pending'
+            });
+
+            await dummyQueue.add('dummyQueue', {
+                event_id: eventId.toString(),
+                payload: eventData,
+                endpoint_id: endpoint.id.toString()
+            });
+        }
+
+        return reply.code(201).send({
+            success: true,
+            message: 'Event received',
+            event_id: eventId
+        });
+    } catch (error) {
+        return reply.code(500).send({
+            success: false,
+            message: 'Failed to process event',
+            error: error?.message,
+            stack: error.stack
+        });
+    }
+}
