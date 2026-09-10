c951c1e7211b39471b1d14146a65ca6ba4bcd887
MaroB05 <s-marwan.hussien@zewailcity.edu.eg>
2026-09-10 09:04:52 +0300

    disable `enableOfflineQueue`

--- src/queue/queue.js
+++ src/queue/queue.js
@@ -11,6 +11,7 @@ export const producerConnection = new IORedis(process.env.REDIS_URL, {
     maxRetriesPerRequest: parseInt(process.env.PRODUCER_MAX_RETRIES_PER_REQUEST) || 3,
     connectTimeout: parseInt(process.env.REDIS_CONNECTION_TIMEOUT) || 10000,
     commandTimeout: parseInt(process.env.COMMAND_TIMEOUT) || 5000,
+    enableOfflineQueue: false, // Disable the offline queue to prevent commands from being queued when the connection is down
 });
 
 export const workerConnection = new IORedis(process.env.REDIS_URL, {
@@ -27,4 +28,10 @@ workerConnection.on('error', (err) => {
 
 export const QUEUE_NAME = 'deliveryQueue';
 
-export const deliveryQueue = new Queue(QUEUE_NAME, { connection: producerConnection });
+export const deliveryQueue = new Queue(QUEUE_NAME, { 
+    connection: producerConnection,
+    defaultJobOptions: {
+        removeOnComplete: {count: parseInt(process.env.REMOVE_ON_COMPLETE_COUNT) || 1000},
+        removeOnFail: {count: parseInt(process.env.REMOVE_ON_FAIL_COUNT) || 5000},
+    }
+});
